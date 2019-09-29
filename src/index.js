5c311a89d064813e1375f11fe3c6340e1d7afba3
santiago fraire <santiwilly@gmail.com>
2019-09-29 13:00:15 +0200

    feat: new faq

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import "./styles/styles.scss";
+
+const accordionBehavior = () => {
+  const accordionLinks = document.getElementsByClassName("faq-link");
+
+  Array.prototype.map.call(accordionLinks, accordionLink => {
+    const target = accordionLink.getAttribute("data-target");
+    accordionLink.onclick = () => {
+      const accordionBlock = document.getElementById(target);
+      if (accordionBlock.classList.contains("show")) {
+        accordionBlock.classList.remove("show");
+      } else {
+        accordionBlock.classList.add("show");
+      }
+    };
+  });
+};
+
+accordionBehavior();
