b1972d7a9c45931c7961cfe4074aadc1384fdb1e
carme <carmecodina.p@gmail.com>
2022-04-17 13:19:41 +0200

    add custom theme

--- pyweb/src/quasar-user-options.js
+++ pyweb/src/quasar-user-options.js
@@ -1,12 +1,11 @@
-
-import 'quasar/dist/quasar.css'
-import lang from 'quasar/lang/en-US.js'
-import '@quasar/extras/material-icons/material-icons.css'
+import "quasar/dist/quasar.css";
+import lang from "quasar/lang/en-US.js";
+import "@quasar/extras/material-icons/material-icons.css";
+import "./css/custom.css";
 
 // To be used on app.use(Quasar, { ... })
 export default {
-  config: {},
-  plugins: {
-  },
-  lang: lang
-}
+    config: {},
+    plugins: {},
+    lang: lang,
+};
