954daa5b51356960f77cca333a2a59ad355f8f95
EstherJP <alu0101102498@gmail.com>
2022-04-17 17:14:21 +0100

    Añadiendo id en seccion evento

--- pyweb/src/components/Evento.vue
+++ pyweb/src/components/Evento.vue
@@ -1,81 +1,83 @@
 <template>
-  <div class="q-pa-xl">
-    <div class="column items-center">
-      <div class="col" style="width: 120vh;">
-        <p>
-          <span class="text-h5 text-weight-bold" style="color: var(--q-primary)">EVENTO</span>
-        </p>
-        <p class="text-justify">
-          {{ $store.state.evento }}
-        </p>
-      </div>
+  <section id="el-evento">
+    <div class="q-pa-xl">
+      <div class="column items-center">
+        <div class="col" style="width: 120vh;">
+          <p>
+            <span class="text-h5 text-weight-bold" style="color: var(--q-primary)">EVENTO</span>
+          </p>
+          <p class="text-justify">
+            {{ $store.state.evento }}
+          </p>
+        </div>
 
-      <div class="col q-pt-sm" style="width: 120vh;">
-        <div class="row justify-center q-gutter-md">
-          <q-card class="col card-border">
-            <q-card-section align="center">
-              <img 
-                src="@/assets/python_comunidad.svg"
-                class="q-pt-sm"
-                width="100" 
-                height="100" 
-              />
+        <div class="col q-pt-sm" style="width: 120vh;">
+          <div class="row justify-center q-gutter-md">
+            <q-card class="col card-border">
+              <q-card-section align="center">
+                <img 
+                  src="@/assets/python_comunidad.svg"
+                  class="q-pt-sm"
+                  width="100" 
+                  height="100" 
+                />
 
-              <div class="text-h6 q-pt-sm">COMUNIDAD</div>
+                <div class="text-h6 q-pt-sm">COMUNIDAD</div>
+                
+                <p class="q-pt-sm">
+                  {{ $store.state.lorem }}
+                </p>
+              </q-card-section>
+
+            </q-card>
+
+            <q-card class="col card-border">
+              <q-card-section align="center">
+                <img 
+                  src="@/assets/open_source.svg"
+                  class="q-pt-sm"
+                  width="100" 
+                  height="100"
+                >
+                
+                <div class="text-h6 q-pt-sm">OPEN SOURCE</div>
+                
+                <p class="q-pt-sm"> 
+                  {{ $store.state.lorem }}
+                </p>
+              </q-card-section>
+            </q-card>
+
+            <q-card class="col card-border">
+              <q-card-section align="center">
+                <img 
+                  src="@/assets/naturaleza.svg"
+                  class="q-pt-sm"
+                  width="100" 
+                  height="100"
+                >
+                
+                <div class="text-h6 q-pt-sm">NATURALEZA</div>
               
-              <p class="q-pt-sm">
-                {{ $store.state.lorem }}
-              </p>
-            </q-card-section>
+                <p class="q-pt-sm">
+                  {{ $store.state.lorem }}
+                </p>
+              </q-card-section>
+            </q-card>
+          </div>
+        </div>
 
-          </q-card>
-
-          <q-card class="col card-border">
-            <q-card-section align="center">
-              <img 
-                src="@/assets/open_source.svg"
-                class="q-pt-sm"
-                width="100" 
-                height="100"
-              >
-              
-              <div class="text-h6 q-pt-sm">OPEN SOURCE</div>
-              
-              <p class="q-pt-sm"> 
-                {{ $store.state.lorem }}
-              </p>
-            </q-card-section>
-          </q-card>
-
-          <q-card class="col card-border">
-            <q-card-section align="center">
-              <img 
-                src="@/assets/naturaleza.svg"
-                class="q-pt-sm"
-                width="100" 
-                height="100"
-              >
-              
-              <div class="text-h6 q-pt-sm">NATURALEZA</div>
-            
-              <p class="q-pt-sm">
-                {{ $store.state.lorem }}
-              </p>
-            </q-card-section>
-          </q-card>
+        <div class="col q-pt-md" style="width: 120vh;">
+          <p>
+            <span class="text-h6 text-weight-bold" style="color: var(--q-primary)">¿A QUIÉN VA DIRIGIDO?</span>
+          </p>
+          <p class="text-justify">
+            {{ $store.state.evento }}
+          </p>
         </div>
       </div>
-
-      <div class="col q-pt-md" style="width: 120vh;">
-        <p>
-          <span class="text-h6 text-weight-bold" style="color: var(--q-primary)">¿A QUIÉN VA DIRIGIDO?</span>
-        </p>
-        <p class="text-justify">
-          {{ $store.state.evento }}
-        </p>
-      </div>
     </div>
-  </div>
+  </section>
 </template>
 
 <style scoped lang="scss">
