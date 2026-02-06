53eea5a4f57aa082e7fe3ab2580d1146e9193d3b
olgararo <olgararo1402@gmail.com>
2026-02-06 19:10:48 +0100

    feat: configure routes in App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Event from './pages/Event/Event'
+import Diversity from './pages/Diversity/Diversity'
+import Faqs from './pages/Faqs/Faqs'
+import PreviousEditions from './pages/PreviousEditions/PreviousEditions'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/event" element={<Event />} />
+        <Route path="/diversity" element={<Diversity />} />
+        <Route path="/faqs" element={<Faqs />} />
+        <Route path="/previous-editions" element={<PreviousEditions />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
