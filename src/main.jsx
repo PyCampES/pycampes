36d41f7a00f2b39f3b89572169ab1172a63b5a28
olgararo <olgararo1402@gmail.com>
2026-02-06 18:44:12 +0100

    feat: define folder structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './styles/global.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
