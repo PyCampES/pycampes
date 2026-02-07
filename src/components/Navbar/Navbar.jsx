e07a7fddd02a64bf22e6fa2752c8b83bcc23e8b7
olgararo <olgararo1402@gmail.com>
2026-02-07 17:48:58 +0100

    feat: create navbar component

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <Link to="/" className="navbar-logo">
+          <img src="../../src/assets/logos/pycamp-logo.webp" alt="PyCamp España" />
+        </Link>
+
+        <ul className="navbar-links">
+          <li>
+            <Link to="/" className="navbar-link active">
+              Inicio
+            </Link>
+          </li>
+          <li>
+            <Link to="/event" className="navbar-link">
+              Próximo PyCamp
+            </Link>
+          </li>
+          <li>
+            <Link to="/previous-editions" className="navbar-link">
+              Ediciones anteriores
+            </Link>
+          </li>
+          <li>
+            <Link to="/diversity" className="navbar-link">
+              Código de conducta
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,14 @@
+import Navbar from '../../components/Navbar/Navbar'
 import './Home.css'
 
 function Home() {
   return (
     <div className="home">
+      <Navbar />
+      <main>
       <h1>PyCamp España</h1>
       <p>Página de inicio</p>
+      </main>
     </div>
   )
 }
