9fdf1390bce2072168a722dd09557062a3deb76c
olgararo <olgararo1402@gmail.com>
2026-02-09 13:22:20 +0100

    refactor: add local fonts and delete google fonts

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './styles/fonts.css'   
 import './styles/variables.css'
 import './styles/global.css'
-import App from './App.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
